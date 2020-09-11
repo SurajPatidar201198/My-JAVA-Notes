632883b06894ce80bb5e1a009ca263f0ca00543c
Suraj Patidar <42700950+SurajPatidar201198@users.noreply.github.com>
2020-09-11 16:38:40 +0530

    toString() and hashCode() done

---
+++ Practice Programs/p1.java
@@ -0,0 +1,12 @@
+class p1
+{
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        arr[1]=23;
+        arr[3]=33;
+        for(int i=0 ;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
