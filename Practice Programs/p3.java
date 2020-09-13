53892d733a42d8da39a04d610be89417ac855cd8
Suraj Patidar <42700950+SurajPatidar201198@users.noreply.github.com>
2020-09-13 22:28:56 +0530

    some practice chapter 1 revision

---
+++ Practice Programs/p3.java
@@ -0,0 +1,9 @@
+class P3
+{
+    public static void main(String[] args) {
+        int suraj=0;
+        double d = 1.2;
+        System.out.println(suraj);
+        System.out.println(d);
+    }
+}
