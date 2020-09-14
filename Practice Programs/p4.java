3100c24db0097ea7701244453b2ba67e17cc79e0
Suraj Patidar <42700950+SurajPatidar201198@users.noreply.github.com>
2020-09-14 10:13:39 +0530

    main method concept completed

---
+++ Practice Programs/p4.java
@@ -0,0 +1,22 @@
+// class p4 {
+//     public static void main(String... args) {
+//         System.out.println("String Main is Working");
+
+//     }
+// }
+// class Child extends p4
+// {
+//     public static void main(String[] args) {
+//         System.out.println("Child class main");
+//     }
+// }
+class Parent
+{
+    static
+    {
+        System.out.println("Static Block");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+}
