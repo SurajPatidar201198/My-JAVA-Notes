f17518980eed93ebba8497d1a8729d100fbd7d2f
Suraj Patidar <42700950+SurajPatidar201198@users.noreply.github.com>
2020-09-16 16:00:44 +0530

    flow control started if-else done

--- Practice Programs/p5.java
+++ Practice Programs/p5.java
@@ -1,6 +1,10 @@
-public class P5
+class P5
 {
     public static void main(String[] args) {
-        
+        if(true)
+            System.out.println("Suraj");
+            System.out.println("Hello");
+        else
+            System.out.println("Hi");
     }
 }
