d831625c0d1acd5d17290d3e023f682c8d0094a1
Suraj Patidar <42700950+SurajPatidar201198@users.noreply.github.com>
2020-09-20 09:04:41 +0530

    Exception handling concept half done

---
+++ Practice Programs/p6.java
@@ -0,0 +1,27 @@
+
+
+
+public class p6 extends RuntimeException
+{
+    static ArithmeticException e ;
+    public static void main(String[] args) {
+        // PrintWriter pw = new PrintWriter("abc.txt");
+        // pw.println("Hello");
+        // System.out.println("Done");
+        // System.out.println("Start");
+        // try
+        // {
+            // System.out.println(10/0);
+        // }
+        // catch(ArithmeticException e)
+        // {
+        //     // System.out.println(10/2);
+        //     // e.printStackTrace();
+        //     // System.out.println(e);
+        //     System.out.println(e.getMessage());
+        // }
+        // System.out.println("End");
+            throw new p6();
+        
+    }
+}
